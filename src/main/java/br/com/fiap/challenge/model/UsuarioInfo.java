b51dcf9efc8399b25a611d0ae461f71f0b59ff49
Gustavo-Nascimento503 <gustavonascimento6631@gmail.com>
2024-03-26 19:25:20 -0300

    Adicionado

---
+++ src/main/java/br/com/fiap/challenge/model/UsuarioInfo.java
@@ -0,0 +1,62 @@
+package br.com.fiap.challenge.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TB_USUARIO_INFO")
+public class UsuarioInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USUARIO_INFO")
+    private Long id;
+
+    @NotBlank(message = "O cpf dever 11 digitos")
+    @Size(min = 11, max = 11, message = "O CPF deve ter 11 numeros")
+    @Column(name = "CPF_USUARIO")
+    private String cpf;
+
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(min = 8, max = 8, message = "O CEP deve ter 8 numeros")
+    @Column(name = "CEP_USUARIO")
+    private String cep;
+
+    @Positive(message = "O numero deve ser positivo")
+    @NotNull
+    @Column(name = "NUM_USUARIO")
+    private int numero;
+
+    @NotBlank(message = "O complemento não pode estar em branco")
+    @Column(name = "COM_USUARIO")
+    private String complemento;
+
+    @NotBlank(message = "O logradouro não pode estar em branco")
+    @Column(name = "LOG_USUARIO")
+    private String logradouro;
+
+    @NotBlank(message = "O telefone não pode estar em branco")
+    @Column(name = "TEL_USUARIO")
+    private String telefone;
+
+    @OneToOne
+    @JoinColumn(name = "ID_USUARIO")
+    private Usuario usuario;
+    
+}
